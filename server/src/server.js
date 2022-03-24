be021a81155e01005824bbca36fcd1032307c153
Naveen <nk0187912@gmail.com>
2022-03-24 16:31:52 +0530

    main-server

---
+++ server/src/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const PORT = process.env.PORT || 8000;
+
+const app = require('./app');
+
+const server = http.createServer(app);
+const { loadHabitablePlanets } = require('./models/planets.model');
+
+async function startSever() {
+   await loadHabitablePlanets();
+   server.listen(PORT, ()=>console.log(`Listening on PORT ${PORT}...`));
+}
+
+startSever();
