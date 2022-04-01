e5494199f1c3b83c321602b4db236999681d54ff
Naveen <nk0187912@gmail.com>
2022-04-01 11:16:52 +0530

    main server

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,6 @@ const http = require('http');
 const PORT = process.env.PORT || 8000;
 
 const app = require('./app');
-
 const server = http.createServer(app);
 
 const { loadHabitablePlanets } = require('./models/planets.model');
