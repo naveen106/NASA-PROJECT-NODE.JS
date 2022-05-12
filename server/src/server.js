814e08c24e8d61f307ac071872d57815549eec9c
Naveen <nk0187912@gmail.com>
2022-05-12 12:11:42 +0530

    added mongoose to connect to Mongo database

--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,33 @@
+require('dotenv').config();
+
 const http = require('http');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
+
+const mongoose = require('mongoose');
+
+//remember to keep .env file in root of your project for this to work.
+const MONGO_URL = process.env.MONGO_URL;
 
 const app = require('./app');
 const server = http.createServer(app);
 
 const { loadHabitablePlanets } = require('./models/planets.model');
 
+//mongoose.connection is an event-emitter, so we can use once() and on() with it.
+mongoose.connection.once('open', () => console.log("MongoDB connection established successfully!"));
+mongoose.connection.on('error', error => console.error(error))
+
 async function startSever() {
+
+   const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+   };
+
+   await mongoose.connect(MONGO_URL, options);
    await loadHabitablePlanets();
+
    server.listen(PORT, ()=>console.log(`Listening on PORT ${PORT}...`));
 }
 
 startSever();
-
-/*
- const PORT = 8000;
- const http = require('http');
- const app = requrie('./app');
- const server = http.createServer(app);
-
- server.listen(PORT, ()=>console.log(`Listening on ${PORT}...`));
-
-*/
